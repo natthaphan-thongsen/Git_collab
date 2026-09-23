5afb7123d39cc0c85139f646d8205ef1c0d5c92e
CHAT <69010180@kmitl.ac.th>
2026-09-23 17:56:43 +0700

    update from a hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, Git Collaboration!\n");
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
     return 0;
 }
 
+int main(void)
+{
+    greeting("Developer A, B, C !!");
+    return 0;
+}
